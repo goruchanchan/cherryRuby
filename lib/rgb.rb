9ca7bc9941e6da97eb39c96717e7d561ef723f0b
goruchan <ppcfr481@yahoo.co.jp>
2022-03-05 15:55:32 +0900

    リファクタリング
    sumメソッドを用いてさらにコードを短縮。sumメソッドで各引数を連結

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,7 +1,6 @@
 def to_hex(r, g, b)
   hex_string = '#'
-  [r, g, b].each do |val|
-    hex_string += val.to_s(16).rjust(2, '0')
+  [r, g, b].sum('#') do |val|
+    val.to_s(16).rjust(2, '0')
   end
-  hex_string
 end
