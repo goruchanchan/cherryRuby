88395603ca49794f7aee006c1a4e5bb914fdb16f
goruchan <ppcfr481@yahoo.co.jp>
2022-03-05 15:51:40 +0900

    リファクタリング
    eachメソッドでドライにする

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,6 +1,7 @@
 def to_hex(r, g, b)
-  '#' +
-  r.to_s(16).rjust(2, '0') +
-  g.to_s(16).rjust(2, '0') +
-  b.to_s(16).rjust(2, '0')
+  hex_string = '#'
+  [r, g, b].each do |val|
+    hex_string += val.to_s(16).rjust(2, '0')
+  end
+  hex_string
 end
