4c0f721c744e91000f0b3d46deba199104b34a76
goruchan <ppcfr481@yahoo.co.jp>
2022-03-12 22:41:52 +0900

    [clean]リファクタリング
    一時変数を使わないようにした。ループカウンタをwith_indexに変更

--- lib/rainbowable.rb
+++ lib/rainbowable.rb
@@ -1,20 +1,10 @@
 module Rainbowable
   def rainbow
-    #1. to_sで自分自身をstring型に
-    str = self.to_s
-
-    # 色つけ順に格納する配列を用意
-    count = 0
     colored_chars = []
-
-    #2. 取得した文字列を1文字ずつループ処理する
-    str.each_char do |char|
+    to_s.each_char.with_index do |char, count|
       color = 31 + count % 6
-      # 各文字の手前にANSIエスケープシーケンスを付与して、配列に追加
       colored_chars << "\e[#{color}m#{char}"
-      count += 1
     end
-    ret = colored_chars.join
-    ret += "\e[0m"
+    colored_chars.join + "\e[0m"
   end
 end
